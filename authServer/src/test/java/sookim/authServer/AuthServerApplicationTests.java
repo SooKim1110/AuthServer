6f524286c47ff0854a3d82d0645bd436c1c2c539
SooKim1110 <ohjskim@naver.com>
2021-12-14 15:38:06 +0900

    Initial Commit

---
+++ authServer/src/test/java/sookim/authServer/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sookim.authServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
